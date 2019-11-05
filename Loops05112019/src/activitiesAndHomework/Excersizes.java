4cb95a25f90d9a8dea3258c551f77bb0edebfe07
Jake <jake@places.com>
2019-11-05 20:50:15 +0200

    A shit load 05/11/2019

---
+++ Loops05112019/src/activitiesAndHomework/Excersizes.java
@@ -0,0 +1,92 @@
+package activitiesAndHomework;
+
+public class Excersizes {
+
+	public static void main(String[] args) {
+
+		// PreQ3
+//		 Random rand = new Random();
+//		int num = rand.nextInt(100);
+//		if (num < 50) {
+//			System.out.println("Small!");
+//			if (num % 2 == 0) {
+//				System.out.println("Even!");
+//			} else {
+//				System.out.println("Odd");
+//			}
+//		} else {
+//			System.out.println("big! ");
+//			if (num % 2 == 0) {
+//				System.out.println("Even!");
+//			} else {
+//				System.out.println("Odd");
+//			}
+//
+//		}
+
+		// PreQ2
+		// Random rand = new Random();
+//		int num = rand.nextInt(100);
+//		if (num > 50) {
+//			System.out.println("Big! " + num);
+//		} else if (num < 50) {
+//			System.out.println("Small!! " + num);
+//		} else {
+//			System.out.println("Bingo! " + num);
+//		}
+
+		// PreQ1
+		// Random rand = new Random();
+//		int num = rand.nextInt(99) + 1;
+//		int num2 = rand.nextInt(99) + 1;
+//		if (num > num2) {
+//			System.out.println(num);
+//		} else {
+//			System.out.println(num2);
+//		}
+
+		// Q4
+		// Random rand = new Random();
+//		int max = rand.nextInt(99) + 1;
+//		int den = rand.nextInt(99) + 1;
+//		int counter = 0;
+//		for (int i = 0; i < max; i++) {
+//			if (i % den == 0) {
+//				counter++;
+//				System.out.println("This Works " + i);
+//			}
+//
+//		}
+//		System.out.println(counter);
+		// Q3
+		// Random rand = new Random();
+//		int num = rand.nextInt(99) + 1;
+//		for (int i = 0; i < num; i += 2) {
+//			System.out.println("These even numbers " + i);
+//		}
+
+		// Q2
+		// Random rand = new Random();
+//		int num1 = rand.nextInt(99) + 1;
+//		int num2 = rand.nextInt(99) + 1;
+//		int temp = 0;
+//		if (num1 > num2) {
+//			for (int i = num2; i < num1; i++) {
+//				System.out.println("number is " + i);
+//			}
+//
+//		} else {
+//			for (int i = num1; i < num2; i++) {
+//				System.out.println("number is " + i);
+//			}
+//		}
+
+//		Random rand = new Random();
+//		int num = rand.nextInt(99) + 1;
+//		for (int i = 0; i < num; i++) {
+//			System.out.println("The numbers " + i);
+//		}
+
+	}
+
+}
---
+++ Loops05112019/src/activitiesAndHomework/Loops1.java
@@ -0,0 +1,25 @@
+package activitiesAndHomework;
+
+public class Loops1 {
+
+	public static void main(String[] args) {
+
+		// Scanner input = new Scanner(System.in);
+//		String name = input.nextLine();
+//		while (!(name.charAt(0) == 'b') && (name.charAt(name.length() - 1) == 'a')) {
+//			System.out.println("Enter name ");
+//			name = input.nextLine();
+//		}
+//		System.out.println("Hello " + name);
+//		input.close();
+
+		// Scanner input = new Scanner(System.in);
+//		String name = input.nextLine();
+//		while (!(name.endsWith("Cohen") && (name.startsWith("Jacob")))) {
+//			System.out.println("Enter Name : ");
+//			name = input.nextLine();
+//		}
+//		System.out.println("Hello " + name);
+	}
+
+}
